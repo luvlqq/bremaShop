7e484ab35e4b673671168a94657d40215042ae76
luvlqq <lualgod92@gmail.com>
2023-03-13 21:23:01 +0300

    cart, user, product init cart, user, product

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { CartModule } from './cart/cart.module';
+import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, CartModule, ProductModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
