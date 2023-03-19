f07c826f72b490eff486618d4212d917400accfc
luvlqq <lualgod92@gmail.com>
2023-03-19 18:53:48 +0300

    swagger connect connect a swager to project for REST api

--- packages/server/src/cart/cart.controller.ts
+++ packages/server/src/cart/cart.controller.ts
@@ -1,6 +1,8 @@
 import { Controller } from '@nestjs/common';
 import { CartService } from './cart.service';
+import {ApiTags} from "@nestjs/swagger";
 
+@ApiTags('Cart')
 @Controller('cart')
 export class CartController {
   constructor(private readonly cartService: CartService) {}
--- packages/server/src/product/product.controller.ts
+++ packages/server/src/product/product.controller.ts
@@ -1,6 +1,8 @@
 import { Controller } from '@nestjs/common';
 import { ProductService } from './product.service';
+import {ApiTags} from "@nestjs/swagger";
 
+@ApiTags('Product')
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
